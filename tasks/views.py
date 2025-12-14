cee38896fedc6f31c9032bfb64190ccc114d76b7
Yeacin <yeacin.santo@gmail.com>
2025-12-14 15:56:55 +0100

    django form

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
+from tasks.forms import TaskForm
+from tasks.models import Employee
 # Create your views here.
 
 def manager_dashboard(request):
@@ -16,3 +16,10 @@ def test(request):
         "names" : ["Mahmud", "Ahmed", "Jhon"]
     }
     return render(request, "test.html" ,context)
+
+
+def create_task(request):
+    employees = Employee.objects.all()
+    form = TaskForm(employees=employees)
+    context = {"form":form}
+    return render(request, "task_form.html",context)

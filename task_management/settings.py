c7479c3a2f14c38875d47de53686d77712ec2bce
Yeacin <yeacin.santo@gmail.com>
2025-12-14 01:40:36 +0100

    5.5

--- task_management/settings.py
+++ task_management/settings.py
@@ -74,10 +74,23 @@ WSGI_APPLICATION = "task_management.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# for SOLite
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+# for PostgreSQL
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'task_management',
+        'USER': 'postgres',
+        'PASSWORD': 'Yeacin@123',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 

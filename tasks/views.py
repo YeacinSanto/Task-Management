eb61fd8658c3639a9b1f5e8475147f7ff0efced9
Yeacin <yeacin.santo@gmail.com>
2025-12-15 02:11:48 +0100

    forms and models forms

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from tasks.forms import TaskForm
+from tasks.forms import TaskForm, TaskModelForm
 from tasks.models import Employee,Task
 # Create your views here.
 
@@ -20,23 +20,30 @@ def test(request):
 
 def create_task(request):
     employees = Employee.objects.all()
-    form = TaskForm(employees=employees) #for GET
+    form = TaskModelForm() #for GET
     
     if request.method == "POST":
-        form = TaskForm(request.POST,employees=employees)
+        form = TaskModelForm(request.POST)
         if form.is_valid():
-            data = form.cleaned_data
-            title = data.get("title")
-            description = data.get("description")
-            due_date = data.get("due_date")
-            assigned_to = data.get("assigned_to")
             
-            task = Task.objects.create(title=title, description=description, due_date=due_date)
+            '''For Model form Data'''
+            form.save()
             
-            # assign employee to task
-            for emp_id in assigned_to:
-                employee = Employee.objects.get(id = emp_id)
-                task.assigned_to.add(employee)
+            return render(request,"task_form.html",{"form":form, "message":"task added successfully"})
+            
+            '''for django form data'''
+            # data = form.cleaned_data
+            # title = data.get("title")
+            # description = data.get("description")
+            # due_date = data.get("due_date")
+            # assigned_to = data.get("assigned_to")
+            
+            # task = Task.objects.create(title=title, description=description, due_date=due_date)
+            
+            # # assign employee to task
+            # for emp_id in assigned_to:
+            #     employee = Employee.objects.get(id = emp_id)
+            #     task.assigned_to.add(employee)
             
             return HttpResponse("Task Added successfully")
             

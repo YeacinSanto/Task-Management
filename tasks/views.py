a0467cafa260030582d570d28e3eae14608d8b97
Yeacin <yeacin.santo@gmail.com>
2025-12-19 00:59:50 +0100

    advance query

--- tasks/views.py
+++ tasks/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from tasks.forms import TaskForm, TaskModelForm
 from tasks.models import *
 from datetime import date
+from django.db.models import Q
 
 # Create your views here.
 
@@ -45,6 +46,26 @@ def view_task(request):
     # tasks = Task.objects.filter(due_date = date.today())
     
     # show the task that priority not low
-    tasks = TaskDetail.objects.exclude(priority="L")
+    # tasks = TaskDetail.objects.exclude(priority="L")
+    
+    '''show task that contain specific word with and(,)'''
+    
+    # tasks = Task.objects.filter(title__icontains="c",status="PENDING")
+    
+    '''Task with or condition'''
+    # tasks = Task.objects.filter(Q(status="PENDING") | Q(status="IN_PROGRESS"))
+    
+    """Advance"""
+    # select_related (foreign_key, OneToOneField)
+    # tasks = TaskDetail.objects.select_related("task").all()
+    
+    # foreign key
+    # tasks = Task.objects.select_related("project").all()
+    # in select_related works one way in manytomany field. but not work in both way
+    '''prefetch_related(reverse foreign key, manytomany)'''
+    # tasks = Project.objects.prefetch_related("task_set").all()
+   
+    # how many employee in a specific task
+    tasks = Task.objects.prefetch_related("assigned_to").all()
     
     return render(request, "show_task.html",{"tasks":tasks})
